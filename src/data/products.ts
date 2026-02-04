8682749b700adfad64e05abc81a1df9e058bb92f
Your Name <youremail@example.com>
2026-02-04 13:35:13 +0530

    commit

--- src/data/products.ts
+++ src/data/products.ts
@@ -21,7 +21,7 @@ export const products: Product[] = [
     name: "Single umbrella Model",
     price: 350,
     description: "Perfectly fitted with intricate detailing. From simple elegance to designer patterns.",
-    image: "/Single.jpeg",
+    image: "/Single u.jpeg",
     category: "Modern",
   },
   {
